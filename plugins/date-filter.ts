b3f851f6e1fd0457dc2e9131231a7c5d95452cfb
Levhita <levhita@gmail.com>
2021-06-07 19:47:54 -0500

    * removed unneded console.log * removed unused import

--- plugins/date-filter.ts
+++ plugins/date-filter.ts
@@ -3,7 +3,6 @@ import moment from 'moment'
 
 export default Vue.filter('formatDate', (value: any) => {
     if (value) {
-        console.log(value)
         return moment(String(value), "YYYY-MM-DD hh:mm:ss").format('DD/MM/YYYY')
     }
 });
